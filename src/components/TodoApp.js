49d66495a24b3d31e651f094e9fcf7df7d2f2b42
RdotSilva <RyandotSilva@gmail.com>
2021-06-09 19:26:31 -0400

    Add handleNewTodoChange method

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -12,6 +12,10 @@ export default class TodoApp extends Component {
       currentTodo: "",
       todos: [],
     };
+
+    const handleNewTodoChange = (event) => {
+      this.setState({ currentTodo: event.target.value });
+    };
   }
 
   render() {
@@ -20,7 +24,10 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
-            <TodoForm currentTodo={this.state.currentTodo} />
+            <TodoForm
+              currentTodo={this.state.currentTodo}
+              handleNewTodoChange={this.handleNewTodoChange}
+            />
           </header>
           <section className="main">
             <TodoList todos={this.state.todos} />
