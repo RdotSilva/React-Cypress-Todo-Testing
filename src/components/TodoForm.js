50196259a0b37952e9dbc0db481b1c271525b6d1
RdotSilva <RyandotSilva@gmail.com>
2021-06-08 20:44:07 -0400

    Refactor TodoForm component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-export default (props) => (
-  <form>
-    <input
-      type="text"
-      autoFocus
-      className="new-todo"
-      placeholder="What needs to be done?"
-    />
-  </form>
-);
+const TodoForm = (props) => {
+  return (
+    <form>
+      <input
+        type="text"
+        autoFocus
+        className="new-todo"
+        placeholder="What needs to be done?"
+      />
+    </form>
+  );
+};
+
+export default TodoForm;
