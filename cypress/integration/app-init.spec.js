478a887b1e80a2a1fc52440bcc1daca6ed24a1d1
RdotSilva <RyandotSilva@gmail.com>
2021-06-12 08:37:48 -0400

    Add custom command

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,10 +1,6 @@
 describe("App initialization", () => {
   it.only("Loads todos on page load", () => {
-    // STUB API call and populate list of todos
-    cy.server();
-    cy.route("GET", "/api/todos", "fixture:todos");
-    cy.visit("/");
-
+    cy.seedAndVisit();
     cy.get(".todo-list li").should("have.length", 4);
   });
 });
