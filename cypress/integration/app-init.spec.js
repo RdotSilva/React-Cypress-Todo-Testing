76e498cef0f16397a346179e826589611c0562de
RdotSilva <RyandotSilva@gmail.com>
2021-06-12 08:35:04 -0400

    Replace hard coded todos w/ fixture

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,31 +1,8 @@
-const todos = [
-  {
-    id: 1,
-    name: "Buy Milk",
-    isComplete: false,
-  },
-  {
-    id: 2,
-    name: "Buy Eggs",
-    isComplete: false,
-  },
-  {
-    id: 3,
-    name: "Buy Bread",
-    isComplete: false,
-  },
-  {
-    id: 4,
-    name: "Make French Toast",
-    isComplete: false,
-  },
-];
-
 describe("App initialization", () => {
   it.only("Loads todos on page load", () => {
     // STUB API call and populate list of todos
     cy.server();
-    cy.route("GET", "/api/todos", todos);
+    cy.route("GET", "/api/todos", "fixture:todos");
     cy.visit("/");
 
     cy.get(".todo-list li").should("have.length", 4);
