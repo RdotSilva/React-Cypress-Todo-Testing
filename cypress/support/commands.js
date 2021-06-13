d0836437e503fcd7173e0b0a35935e67d85b58c5
RdotSilva <RyandotSilva@gmail.com>
2021-06-13 14:04:57 -0400

    Fix typo

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
-Cypress.Commands.add("seedAndVisit", (seedData = "fixture:todo") => {
-  // STUB API call and populate list of todos
+Cypress.Commands.add("seedAndVisit", (seedData = "fixture:todos") => {
+  // Stub API call and populate list of todos
   cy.server();
   cy.route("GET", "/api/todos", seedData);
   cy.visit("/");
