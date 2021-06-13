734b3954a0f6e46d707ed2a7e0f826e9aac348e0
RdotSilva <RyandotSilva@gmail.com>
2021-06-13 13:55:16 -0400

    Add test for displaying completed items

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -2,4 +2,13 @@ describe("List items", () => {
   beforeEach(() => {
     cy.seedAndVisit();
   });
+
+  it("properly displays completed items", () => {
+    cy.get(".todo-list li")
+      .filter(".completed")
+      .should("have.length", 1)
+      .and("contain", "Eggs")
+      .find(".toggle")
+      .should("be.checked");
+  });
 });
