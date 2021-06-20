30401bd6d849f32d1d4986a3fcabdaf00252e680
RdotSilva <RyandotSilva@gmail.com>
2021-06-20 11:53:07 -0400

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = (props) => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Created to test React components using Cypress</p>
+    </div>
+  );
+};
+
+export default About;
