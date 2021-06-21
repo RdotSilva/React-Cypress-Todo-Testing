6279d99f3d7c68d42217238fc93dfca6e2669133
RdotSilva <RyandotSilva@gmail.com>
2021-06-21 16:43:49 -0400

    Add test for h1 element

--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -5,4 +5,11 @@ describe("About page", () => {
     // Get the container element
     cy.get(".about-container");
   });
+
+  it("Should have an h1 element", () => {
+    // Navigate to about page
+    cy.visit("/about");
+    // Get the h1 element
+    cy.get(".about-header").contains("About page");
+  });
 });
