026e0d4413763eba7e139fbb6f63f3660ae9932b
RdotSilva <RyandotSilva@gmail.com>
2021-06-22 14:32:13 -0400

    Add Welcome component

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = (props) => {
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-header">Welcome to Todo App</h1>
+    </div>
+  );
+};
+
+export default Welcome;
