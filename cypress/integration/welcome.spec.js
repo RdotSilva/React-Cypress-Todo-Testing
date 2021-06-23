b1393e9d17d132826e78ecb68b864a4a16f7c0c5
RdotSilva <RyandotSilva@gmail.com>
2021-06-23 16:18:05 -0400

    Add test for h1 welcome

--- cypress/integration/welcome.spec.js
+++ cypress/integration/welcome.spec.js
@@ -1,8 +1,17 @@
 describe("About page", () => {
   it("Should have a container", () => {
-    // Navigate to about page
+    // Navigate to welcome page
     cy.visit("/welcome");
     // Get the container element using data-test-id
-    cy.get('[data-testid="welcome-container]').click();
+    cy.get('[data-testid="welcome-container]').should("exist");
+  });
+
+  it("Should have an h1 element", () => {
+    it("Should have an h1 element", () => {
+      // Navigate to welcome page
+      cy.visit("/welcome");
+      // Get the h1 element
+      cy.get('[data-testid="welcome-header]').should("exist");
+    });
   });
 });
