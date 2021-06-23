b390be129c40ad2d6370037752b2218e4731e924
RdotSilva <RyandotSilva@gmail.com>
2021-06-23 16:19:44 -0400

    Add button

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,7 @@ const Welcome = (props) => {
   return (
     <div data-testid="welcome-container">
       <h1 data-testid="welcome-header">Welcome to Todo App</h1>
+      <button class="welcome-button">Click Me</button>
     </div>
   );
 };
