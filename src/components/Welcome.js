b48d359b8b83bd22ef99bdfa736ee07c15a58614
RdotSilva <RyandotSilva@gmail.com>
2021-06-23 16:14:50 -0400

    Add data-testid properties

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 const Welcome = (props) => {
   return (
-    <div className="welcome-container">
-      <h1 className="welcome-header">Welcome to Todo App</h1>
+    <div data-testid="welcome-container">
+      <h1 data-testid="welcome-header">Welcome to Todo App</h1>
     </div>
   );
 };
