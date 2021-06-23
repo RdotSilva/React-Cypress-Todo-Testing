25a250852a7e75fda91b05eb8c6bdf2990ba3f89
RdotSilva <RyandotSilva@gmail.com>
2021-06-23 16:18:58 -0400

    Refactor tests using beforeEach method

--- cypress/integration/welcome.spec.js
+++ cypress/integration/welcome.spec.js
@@ -1,15 +1,15 @@
 describe("About page", () => {
-  it("Should have a container", () => {
+  beforeEach(() => {
     // Navigate to welcome page
     cy.visit("/welcome");
+  });
+  it("Should have a container", () => {
     // Get the container element using data-test-id
     cy.get('[data-testid="welcome-container]').should("exist");
   });
 
   it("Should have an h1 element", () => {
     it("Should have an h1 element", () => {
-      // Navigate to welcome page
-      cy.visit("/welcome");
       // Get the h1 element
       cy.get('[data-testid="welcome-header]').should("exist");
     });
